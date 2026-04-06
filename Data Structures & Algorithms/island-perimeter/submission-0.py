9b47f3c2efa91888f35cdb98bfc509347c3acc84
Sandeep Vinay <159754127+Svkayy@users.noreply.github.com>
2026-04-06 00:42:31 -0400

    Add: island-perimeter - submission-0

---
+++ Data Structures & Algorithms/island-perimeter/submission-0.py
@@ -0,0 +1,30 @@
+from collections import deque
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        perimeter = 0
+        neighbors = [(1,0), (-1,0), (0,1), (0,-1)]
+        
+        visited = set()
+        q = deque()
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    q.append((r, c))
+                    visited.add((r, c))
+                    break
+        while q:
+            r, c = q.popleft()
+            if grid[r][c] == 1:
+                perimeter += 4
+                for dr, dc in neighbors:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        perimeter -= 1
+                        if (nr, nc) not in visited:
+                            visited.add((nr, nc))
+                            q.append((nr, nc))
+        
+        return perimeter
+
