6196bdc573317f9bd557b713ebe60ad6086128e3
Sandeep Vinay <159754127+Svkayy@users.noreply.github.com>
2026-04-06 23:29:13 -0400

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.py
@@ -0,0 +1,7 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+        
+        return max(count, key=count.get)
